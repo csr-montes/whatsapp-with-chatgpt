047e98e1440ee5087658b5aa3d65667958f01753
Alvaro Montes <7chetos@gmail.com>
2023-02-20 17:26:18 -0500

    Agregar logica para realizar consultas al API de chatGPT con los mensajes personales escritos en WhatsApp

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const QRCode = require("qrcode");
 const { Client } = require("whatsapp-web.js");
+const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const client = new Client();
+let openaiApiKey = process.env.OPENAI_API_KEY;
 let qrReceived = "-";
-
-app.get("/", (req, res) => {
-    res.send("Hellow world!");
-});
 
 app.get("/qrcode", async (req, res) => {
     try {
         let qrCodeUrl = await generateQRCode(qrReceived);
         res.send(`<img src="${qrCodeUrl}">`);
     } catch (error) {
-        res.status(500).send("Error al generar el código QR");
+        res.status(500).send("Error generating QR code");
     }
 });
 
@@ -35,6 +33,8 @@ function generateQRCode(text) {
 app.listen(port);
 console.log("Server on port", port);
 
+client.initialize();
+
 client.on("qr", (qr) => {
     console.log(qr);
     qrReceived = qr;
@@ -44,16 +44,73 @@ client.on("ready", () => {
     console.log("Client is ready!");
 });
 
-client.on("message_create", (message) => {
-    console.log(message);
-    if (message.fromMe && message.body.substring(0, 4) === "/gpt") {
-        let query = message.body.substring(5);
-        client.sendMessage(message.from, consultTheAPIChatGPT(query));
+client.on("message_create", async (message) => {
+    if (message.fromMe) {
+        const params = isCommandAndExtractParams(message.body);
+        if (params == undefined) {
+            return;
+        }
+
+        if (params.command === "/help") {
+            client.sendMessage(
+                message.from,
+                "*Set OPENAI_API_KEY*\n /key [OPENAI_API_KEY]\n\n" +
+                    "*Set API (soon)*\n /set [number of API]\n\n" +
+                    "*Official API*\n /gpt [query]"
+            );
+            return;
+        }
+
+        if (params.command === "/key") {
+            openaiApiKey = params.query;
+            client.sendMessage(message.from, "registered key");
+            return;
+        }
+
+        if (params.command === "/gpt") {
+            client.sendMessage(
+                message.from,
+                await consultTheAPIChatGPT(params.query)
+            );
+        } else {
+            client.sendMessage(message.from, "command is invalid");
+        }
     }
 });
 
-function consultTheAPIChatGPT(query) {
-    return "RESPUESTA -> " + query;
+function isCommandAndExtractParams(message) {
+    if (message.startsWith("/")) {
+        if (message === "/help") {
+            return { command: message };
+        }
+
+        const endIndex = message.indexOf(" ");
+        const params = {
+            command: message.substring(0, endIndex),
+            query: message.substring(endIndex + 1),
+        };
+
+        console.log(params);
+        return params;
+    }
 }
 
-client.initialize();
+async function consultTheAPIChatGPT(query) {
+    const configuration = new Configuration({
+        apiKey: openaiApiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: query,
+            temperature: 0,
+            max_tokens: 500,
+        });
+        console.log(response.data.choices[0]);
+        return response.data.choices[0].text;
+    } catch (error) {
+        console.log(error);
+        return "Oops, an error occurred";
+    }
+}
