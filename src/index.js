b175051fa7eb459e93f0870a04529c28ede028a9
Alvaro Montes <7chetos@gmail.com>
2023-02-19 22:23:15 -0500

    Agregar visualizacion del codigo qr a un endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const qrcode = require("qrcode");
+const { Client } = require("whatsapp-web.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const client = new Client();
 
 app.get("/", (req, res) => {
     res.send("Hellow world!");
@@ -9,3 +12,40 @@ app.get("/", (req, res) => {
 
 app.listen(port);
 console.log("Server on port", port);
+
+client.on("qr", (qr) => {
+    console.log(qr);
+    app.get("/qrcode", async (req, res) => {
+        try {
+            let qrCodeUrl = await generateQRCode(qr);
+
+            res.send(`<img src="${qrCodeUrl}">`);
+        } catch (error) {
+            res.status(500).send("Error al generar el código QR");
+        }
+    });
+});
+
+function generateQRCode(text) {
+    return new Promise((resolve, reject) => {
+        qrcode.toDataURL(text, (err, url) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(url);
+            }
+        });
+    });
+}
+
+client.on("ready", () => {
+    console.log("Client is ready!");
+});
+
+client.on("message", (message) => {
+    if (message.body === "Hola") {
+        client.sendMessage(message.from, "Respuesta");
+    }
+});
+
+client.initialize();
